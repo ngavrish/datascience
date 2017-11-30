5711a1b7c05605971fd59ac50662cde3319e6167
Nikita Gavrish <nikikikita@gmail.com>
2017-11-30 12:38:22 +0300

    1

--- superdatascience/course/Part1/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
+++ superdatascience/course/Part1/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
@@ -1,21 +1,6 @@
 dataset = read.csv('Data.csv')
+# dataset = dataset[, 2:3]
 
-# missing data
-dataset$Age = ifelse(is.na(dataset$Age),
-                     ave(dataset$Age, FUN = function(x) mean(x, na.rm=TRUE)),
-                     dataset$Age)
-dataset$Salary = ifelse(is.na(dataset$Salary),
-                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm=TRUE)),
-                     dataset$Salary)
-
-# encoding categorical
-dataset$Country = factor(dataset$Country,
-                         levels = c('France', 'Spain', 'Germany'),
-                         labels = c(1,2,3))
-
-dataset$Purchased = factor(dataset$Purchased,
-                         levels = c('Yes', 'No'),
-                         labels = c(1,0))
 # splitting the dataset into training set and test set
 library(caTools)
 set.seed(123)
@@ -24,5 +9,5 @@ training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature scaling
-training_set[, 2:3] = scale(training_set[, 2:3])
-test_set[, 2:3] = scale(test_set[, 2:3])
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
