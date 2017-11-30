05bc3b4bfd27410c54a37e5a27676e4b1a3767c5
Nikita Gavrish <nikikikita@gmail.com>
2017-11-30 12:32:25 +0300

    1

--- superdatascience/src/data_preprocessing_templates.py
+++ superdatascience/src/data_preprocessing_templates.py
@@ -6,20 +6,16 @@ dataset = pd.read_csv('../course/Part1/Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, len(dataset.iloc[0, :])-1].values
 
-print(X)
-print(y)
 # taking care of missing data
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values='NaN', strategy="mean", axis=0)
-imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
-# print(X)
+# from sklearn.preprocessing import Imputer
+# imputer = Imputer(missing_values='NaN', strategy="mean", axis=0)
+# imputer.fit(X[:, 1:3])
+# X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 # Encoding categorical
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelEncoder_X = LabelEncoder()
 X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
-
 onehotencoder = OneHotEncoder(categorical_features= [0])
 X = onehotencoder.fit_transform(X).toarray()
 # print(X)
