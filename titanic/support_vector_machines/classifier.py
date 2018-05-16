e8e5cf7cde09999b6b621874d63278659e78501f
Nikita Gavrish <nikita.gavrish@arrival.com>
2018-05-16 15:16:25 +0300

    initial commit for SVM trying

--- titanic/support_vector_machines/classifier.py
+++ titanic/support_vector_machines/classifier.py
@@ -1,25 +1,7 @@
 # coding=utf8
 
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import xgboost as xgb
-import re
-import seaborn as sns
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import LogisticRegression
-from sklearn.linear_model import SGDClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
-from sklearn.feature_selection import SelectKBest, f_classif
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
-from sklearn.preprocessing import StandardScaler
-from sklearn import metrics
+from sklearn import svm
 
 pd.set_option('display.width', 256)
 
