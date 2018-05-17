7bde78866495f1d28892512a70b450900826fb08
Nikita Gavrish <nikita.gavrish@arrival.com>
2018-05-17 15:48:26 +0300

    import update

--- titanic/support_vector_machines/svm.py
+++ titanic/support_vector_machines/svm.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-train_dataset = pd.read_csv('./datasets/train.csv')
-X_train = train_dataset.iloc[:, [2, 4, 5, 6, 9]].values
+train_dataset = pd.read_csv('./datasets/train.csv').fillna(0)
+X_train = train_dataset.iloc[:, [2, 4, 5]].values
 y_train = train_dataset.iloc[:, 1].values
 
 
-test_dataset = pd.read_csv('./datasets/test.csv')
-X_test = test_dataset.iloc[:, [2, 4, 5, 6, 8, 9]].values
+test_dataset = pd.read_csv('./datasets/test.csv').fillna(0)
+X_test = test_dataset.iloc[:, [1, 3, 4]].values
 y_test = test_dataset.iloc[:, 1].values
 
 # taking care of missing data
@@ -20,6 +20,7 @@ from sklearn_pandas.categorical_imputer import CategoricalImputer
 imputer = CategoricalImputer(missing_values='NaN')
 imputer.fit(X_train, y_train)
 imputer.fit(X_test, y_test)
+
 
 # Categorical data fix
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -49,45 +50,46 @@ classifier.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
+print(str(y_pred))
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-
-cm = confusion_matrix(y_test, y_pred)
-
-# Visualising the Training set results
-from matplotlib.colors import ListedColormap
-
-X_set, y_set = X_train, y_train
-X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
-                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha=0.75, cmap=ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c=ListedColormap(('red', 'green'))(i), label=j)
-plt.title('SVM (Training set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
-
-# Visualising the Test set results
-from matplotlib.colors import ListedColormap
-
-X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
-                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha=0.75, cmap=ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c=ListedColormap(('red', 'green'))(i), label=j)
-plt.title('SVM (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
+# from sklearn.metrics import confusion_matrix
+#
+# cm = confusion_matrix(y_test, y_pred)
+#
+# # Visualising the Training set results
+# from matplotlib.colors import ListedColormap
+#
+# X_set, y_set = X_train, y_train
+# X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+#                      np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
+# plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+#              alpha=0.75, cmap=ListedColormap(('red', 'green')))
+# plt.xlim(X1.min(), X1.max())
+# plt.ylim(X2.min(), X2.max())
+# for i, j in enumerate(np.unique(y_set)):
+#     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+#                 c=ListedColormap(('red', 'green'))(i), label=j)
+# plt.title('SVM (Training set)')
+# plt.xlabel('Age')
+# plt.ylabel('Estimated Salary')
+# plt.legend()
+# plt.show()
+#
+# # Visualising the Test set results
+# from matplotlib.colors import ListedColormap
+#
+# X_set, y_set = X_test, y_test
+# X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+#                      np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
+# plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+#              alpha=0.75, cmap=ListedColormap(('red', 'green')))
+# plt.xlim(X1.min(), X1.max())
+# plt.ylim(X2.min(), X2.max())
+# for i, j in enumerate(np.unique(y_set)):
+#     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+#                 c=ListedColormap(('red', 'green'))(i), label=j)
+# plt.title('SVM (Test set)')
+# plt.xlabel('Age')
+# plt.ylabel('Estimated Salary')
+# plt.legend()
+# plt.show()
