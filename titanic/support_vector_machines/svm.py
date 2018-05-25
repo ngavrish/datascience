ba6d151c58a9360a99b95c5c474d27e3d48c8895
Nikita Gavrish <nikita.gavrish@arrival.com>
2018-05-25 15:12:07 +0300

    siblings dont work

--- titanic/support_vector_machines/svm.py
+++ titanic/support_vector_machines/svm.py
@@ -2,76 +2,98 @@
 
 # Importing the libraries
 import numpy
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 
 # Importing the dataset
 train_dataset = pd.read_csv('./datasets/train.csv')
 test_dataset = pd.read_csv('./datasets/test.csv')
-# class = 0, sex  = 1, age = 2, siblings = 3, parch = 4, fare = 5, embarked = 6
-X_test = test_dataset.iloc[:, [1, 3, 4, 5, 6, 8]].values
-X_train = train_dataset.iloc[:, [2, 4, 5, 6, 7, 9]].values
+
+# adding features
+# 1) adding amount of siblings
+test_dataset["SiblingsAmount"] = test_dataset["SibSp"] + test_dataset["Parch"]
+train_dataset["SiblingsAmount"] = train_dataset["SibSp"] + train_dataset["Parch"]
+# print(X_test["SiblingsAmount"])
+
+sns.pairplot(train_dataset, vars=["Pclass", "SiblingsAmount"], hue="Survived", dropna=True)
+# plt.show()
+
+# data exploration
+print(train_dataset.groupby(["Pclass", "SiblingsAmount"])["Survived"].value_counts(normalize=True))
+
+
+test_selected_features = test_dataset.loc[:, ["Pclass", "Sex", "Age", "Fare", "SiblingsAmount"]]
+train_selected_features = train_dataset.loc[:, ["Pclass", "Sex", "Age", "Fare", "SiblingsAmount"]]
+print(test_selected_features)
+
+# class = 0, sex  = 1, age = 2, fare = 3, siblings_amount = 4
+X_test = test_selected_features.values
+X_train = train_selected_features.values
 y_train = train_dataset.iloc[:, 1].values
+
+
 
 # taking care of missing data
 from sklearn_pandas.categorical_imputer import CategoricalImputer
 categorical_imputer = CategoricalImputer(missing_values='NaN')
 
+# add missing Sex
 categorical_imputer.fit(X_train[:, 1])
 X_train[:, 1] = categorical_imputer.transform(X_train[:, 1])
 
 categorical_imputer.fit(X_test[:, 1])
 X_test[:, 1] = categorical_imputer.transform(X_test[:, 1])
 
-categorical_imputer.fit(X_train[:, 5])
-X_train[:, 5] = categorical_imputer.transform(X_train[:, 5])
-
-categorical_imputer.fit(X_test[:, 5])
-X_test[:, 5] = categorical_imputer.transform(X_test[:, 5])
-
 
 from sklearn.preprocessing import Imputer
-train_imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
-train_imputer.fit(X_train[:, 2:5])
-X_train[:, 2:5] = train_imputer.transform(X_train[:, 2:5])
 
+train_imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
+train_imputer.fit(X_train[:, 2:4])
+X_train[:, 2:4] = train_imputer.transform(X_train[:, 2:4])
 
 test_imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
-test_imputer.fit(X_test[:, 2:5])
-X_test[:, 2:5] = test_imputer.transform(X_test[:, 2:5])
+test_imputer.fit(X_test[:, 2:4])
+X_test[:, 2:4] = test_imputer.transform(X_test[:, 2:4])
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler, MaxAbsScaler
+
 sc = StandardScaler()
 maxAbsScaler = MaxAbsScaler()
 # age scaling
 X_train[:, [2]] = sc.fit_transform(X_train[:, [2]])
 X_test[:, [2]] = sc.transform(X_test[:, [2]])
 # fare scaling
-X_train[:, [5]] = maxAbsScaler.fit_transform(X_train[:, [5]])
-X_test[:, [5]] = maxAbsScaler.transform(X_test[:, [5]])
-# siblings scaling
+X_train[:, [3]] = maxAbsScaler.fit_transform(X_train[:, [3]])
+X_test[:, [3]] = maxAbsScaler.transform(X_test[:, [3]])
+# siblings amount scaling
+# X_train[:, [4]] = maxAbsScaler.fit_transform(X_train[:, [4]])
+# X_test[:, [4]] = maxAbsScaler.transform(X_test[:, [4]])
 
 # Categorical data fix
 df_before_categorical = pd.DataFrame(X_train)
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder= LabelEncoder()
+
+labelencoder = LabelEncoder()
 X_train[:, 1] = labelencoder.fit_transform(X_train[:, 1])
 X_test[:, 1] = labelencoder.fit_transform(X_test[:, 1])
 
-#X_train[:, 5] = labelencoder.fit_transform(X_train[:, 5])
-#X_test[:, 5] = labelencoder.fit_transform(X_test[:, 5])
+# X_train[:, 5] = labelencoder.fit_transform(X_train[:, 5])
+# X_test[:, 5] = labelencoder.fit_transform(X_test[:, 5])
 
 
 df = pd.DataFrame(X_train)
 
-onehotencoder = OneHotEncoder(categorical_features=[0, 5])
+onehotencoder = OneHotEncoder(categorical_features=[0, 4])
 
-#X_train = onehotencoder.fit_transform(X_train[:, :]).toarray()
-#X_test = onehotencoder.fit_transform(X_test[:, :]).toarray()
+# X_train = onehotencoder.fit_transform(X_train[:, :]).toarray()
+# X_test = onehotencoder.fit_transform(X_test[:, :]).toarray()
 
 # Fitting SVM to the Training set
 from sklearn.svm import SVC
+
 #
 classifier = SVC(kernel='rbf', random_state=0)
 classifier.fit(X_train, y_train)
@@ -80,10 +102,12 @@ classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 
 print(str(y_pred))
+
 submission = pd.DataFrame({
-        "PassengerId": test_dataset["PassengerId"],
-        "Survived": y_pred
-    })
+    "PassengerId": test_dataset["PassengerId"],
+    "Survived": y_pred
+})
+
 submission.to_csv(f"./submission.csv", index=False)
 # Making the Confusion Matrix
 # from sklearn.metrics import confusion_matrix
